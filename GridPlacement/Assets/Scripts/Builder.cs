f4989975b27f5e698d002ad52e8039192c9567ef
Enesozdogan <72387932+Enesozdogan@users.noreply.github.com>
2024-01-26 16:23:15 +0300

    Building Deletion

--- GridPlacement/Assets/Scripts/Builder.cs
+++ GridPlacement/Assets/Scripts/Builder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -9,7 +10,9 @@ public class Builder : MonoBehaviour
     public TileSO tiles;
     private int selectedIndex;
 
-   
+    [SerializeField]
+    private GameObject gridPlane;
+
     public int[,] collisionMat = new int[10, 10];
     private void Start()
     {
@@ -21,8 +24,59 @@ public class Builder : MonoBehaviour
                 collisionMat[i, j] = -1;
         }
     }
+
+    public void StartDeleting()
+    {
+        GetMousePos.isDeleting= true;
+        GetMousePos.isUsingGrid = true;
+
+        GetMousePos.Onclick += DeleteObject;
+        GetMousePos.OnCancel += StopDeleting;
+    }
+
+    private void StopDeleting()
+    {
+        GetMousePos.isUsingGrid = false;
+        GetMousePos.isDeleting = false;
+
+        GetMousePos.targetObject = null; 
+
+        GetMousePos.Onclick -= DeleteObject;
+        GetMousePos.OnCancel -= StopDeleting;
+    }
+
+    private void DeleteObject()
+    {
+        if (GetMousePos.targetObject == null)
+        {
+            Debug.LogError("No Target Object Detected");
+            return;
+        }
+
+        if(GetMousePos.targetObject.TryGetComponent(out ObjectDestroyer objectDestroyer))
+        {
+            Vector3 objPos = GetMousePos.targetObject.transform.position;
+            int i, j;
+        
+            GetObjDecimalCoordinateIndex(objPos, out i , out j);
+            Vector2 buildingSize = tiles.buildings[collisionMat[i, j]].Size;
+
+            MakeCellClear(i,j,buildingSize);
+            objectDestroyer.Destroy();
+        }
+        else
+        {
+            Debug.LogError("Can't Destroy Object");
+            return;
+        }
+            
+
+    }
+
     public void StartPlacing(int id)
     {
+        GetMousePos.isUsingGrid = true;
+
         selectedIndex = tiles.buildings.FindIndex(building => building.Id == id);
         if (selectedIndex == -1)
         {
@@ -37,6 +91,8 @@ public class Builder : MonoBehaviour
 
     public void StopPlacing()
     {
+        GetMousePos.isUsingGrid = false;
+
         selectedIndex = -1;
         GetMousePos.Onclick -= PlaceBuilding;
         GetMousePos.OnCancel -= StopPlacing;
@@ -49,24 +105,35 @@ public class Builder : MonoBehaviour
             return;
         }
 
-        Vector3 placePos = GetMousePos.cursor.transform.position;
-        int i = (int)(placePos.z + 4.5f);
-        int j = (int)(placePos.x + 4.5f);
+        Vector3 placePos;
+        int i, j;
+        GetDecimalCoordinateIndex(out placePos, out i, out j);
 
-        if (!CheckCollision(i,j, tiles.buildings[selectedIndex].Size))
+        if (!CheckCollision(i, j, tiles.buildings[selectedIndex].Size))
         {
             Debug.LogError("Cannot place on this cell");
             return;
         }
-            
+
 
         GameObject building = Instantiate(tiles.buildings[selectedIndex].Prefab);
         building.transform.position = placePos;
 
         MakeCellDirty(i, j, tiles.buildings[selectedIndex].Size, tiles.buildings[selectedIndex].Id);
-        
+
     }
 
+    private void GetDecimalCoordinateIndex(out Vector3 placePos, out int i, out int j)
+    {
+        placePos = GetMousePos.cursor.transform.position;
+        i = (int)(placePos.z + 4.5f);
+        j = (int)(placePos.x + 4.5f);
+    }
+    private void GetObjDecimalCoordinateIndex(Vector3 objPos, out int i, out int j)
+    { 
+        i = (int)(objPos.z + 4.5f);
+        j = (int)(objPos.x + 4.5f);
+    }
     private void MakeCellDirty(int indexI, int indexJ, Vector2 buildingSize, int buildingId)
     {
         for (int i = indexI; i < indexI+buildingSize.y; i++)
@@ -74,6 +141,16 @@ public class Builder : MonoBehaviour
             for (int j = indexJ; j < indexJ+buildingSize.x; j++)
             {
                 collisionMat[i, j] = buildingId;
+            }
+        }
+    }
+    private void MakeCellClear(int indexI, int indexJ, Vector2 buildingSize)
+    {
+        for (int i = indexI; i < indexI + buildingSize.y; i++)
+        {
+            for (int j = indexJ; j < indexJ + buildingSize.x; j++)
+            {
+                collisionMat[i, j] = -1;
             }
         }
     }
@@ -89,5 +166,20 @@ public class Builder : MonoBehaviour
             }
         }
         return true;
+    }
+
+    private void Update()
+    {
+        if(GetMousePos.isUsingGrid && !gridPlane.activeSelf)
+        {
+            gridPlane.SetActive(true);
+            GetMousePos.cursor.SetActive(true);
+        }
+        else if(!GetMousePos.isUsingGrid && gridPlane.activeSelf)
+        {
+            gridPlane.SetActive(false);
+            GetMousePos.cursor.SetActive(false);
+        }
+
     }
 }
--- GridPlacement/Assets/Scripts/GetMousePos.cs
+++ GridPlacement/Assets/Scripts/GetMousePos.cs
@@ -9,14 +9,17 @@ public class GetMousePos : MonoBehaviour
 {
     public Vector3 mousePos;
     [SerializeField]
-    private LayerMask planeLayer;
+    private LayerMask planeLayer,objectLayer;
 
     public GameObject cursor;
     [SerializeField]
     private float cursorOffsetY;
     private Grid grid;
 
+    public bool isUsingGrid;
+    public bool isDeleting;
 
+    public GameObject targetObject;
     public event Action Onclick, OnCancel;
     private void Awake()
     {
@@ -36,11 +39,19 @@ public class GetMousePos : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
             Onclick?.Invoke();
         if(Input.GetMouseButtonDown(1))
-            OnCancel?.Invoke();   
+            OnCancel?.Invoke();
 
-        GetMousePositionWorld();
-        cursor.transform.position = grid.WorldToCell(mousePos);
-        cursor.transform.position = grid.GetCellCenterWorld(Vector3Int.CeilToInt(grid.WorldToCell(mousePos))) - new Vector3(0,0.5f,0);
+        if (isUsingGrid)
+        {
+            if (isDeleting)
+            {
+                targetObject = GetRaycastedObject();
+            }
+            GetMousePositionWorld();
+            cursor.transform.position = grid.WorldToCell(mousePos);
+            cursor.transform.position = grid.GetCellCenterWorld(Vector3Int.CeilToInt(grid.WorldToCell(mousePos))) - new Vector3(0, 0.5f, 0);
+        }
+       
         
 
         
@@ -48,14 +59,29 @@ public class GetMousePos : MonoBehaviour
 
     private void GetMousePositionWorld()
     {
+        //Iki raycasti tek kolda yaz ki hem mouse hareketi hem de obje tespiti yapilsin.
         Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
+     
         if (Physics.Raycast(cameraRay, out hit, 1000, planeLayer))
         {
             mousePos = hit.point;
         }
+
+
     }
 
+    //Mouse pozisyonu raycast edilen objenin pozisyonuna getirilmeli ki matrix dogru coordinattan temizlensin.
+    public GameObject GetRaycastedObject()
+    {
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit2;
+        if (Physics.Raycast(cameraRay, out hit2, 1000, objectLayer))
+        {
+            return hit2.collider.gameObject;
+        }
+        return null;
+    }
     private void OnDrawGizmos()
     {
         Debug.DrawRay(Camera.main.transform.position, (mousePos - Camera.main.transform.position), Color.red);
